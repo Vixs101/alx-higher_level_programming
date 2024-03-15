71eaca5c3fa6153be674b9144942341ec4ea185a
Vixs101 <elijahvix695@gmail.com>
2024-03-15 05:56:21 +0100

    made a few corrections to the program that prints the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,10 +7,10 @@ def main():
 
     if len(argv) == 1:
         print("{}".format(total))
-    else:
-        start_index = 1 if len(argv) == 2 else int(argv[1])
+    elif len(argv) > 1:
+        start_index = 1
         for i in range(start_index, len(argv)):
-            total += int(argv[i])
+            total = total + int(argv[i])
         print("{}".format(total))
 
 
