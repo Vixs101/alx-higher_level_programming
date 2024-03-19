4006d4f0ed8de0367b2dc7198356faa8cf522492
Vixs101 <elijahvix695@gmail.com>
2024-03-19 06:05:00 +0100

    fixed a whitespace issue

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,9 +4,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a + (0, 0)
     if len(tuple_b) == 0:
         tuple_b = tuple_b + (0, 0)
-        
+
     tuple_aM = tuple_a if len(tuple_a) > 1 else tuple_a + (0,)
     tuple_bM = tuple_b if len(tuple_b) > 1 else tuple_b + (0,)
-    
+
     result = (tuple_aM[0] + tuple_bM[0], tuple_aM[1] + tuple_bM[1])
     return result
