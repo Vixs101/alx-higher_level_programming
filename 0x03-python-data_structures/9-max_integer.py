7ba663dafab70acb885613061a2f0ddfffe3f514
Vixs101 <elijahvix695@gmail.com>
2024-03-19 12:25:06 +0100

    added a test to check if the current index is >= to the next

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,14 +2,12 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-
     i = len(my_list) - 1
-
     while i > 0:
         j = 0
         while j < i:
-            if my_list[j] > my_list[j + 1]:
-                a = my_list[j] 
+            if my_list[j] >= my_list[j + 1]:
+                a = my_list[j]
             j += 1
         i -= 1
     return a
