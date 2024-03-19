39e435b3d87acdbe482b518e27d21082d71a44bc
Vixs101 <elijahvix695@gmail.com>
2024-03-19 12:35:31 +0100

    added a test to check and compare the last element in the list with the temp variable

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,12 +2,16 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    i = len(my_list) - 1
+    i = len(my_list)
     while i > 0:
         j = 0
         while j < i:
-            if my_list[j] >= my_list[j + 1]:
-                a = my_list[j]
+            if my_list[j] != my_list[-1]:
+                if my_list[j] >= my_list[j + 1]:
+                    a = my_list[j]
+            else:
+                if a >= my_list[j]:
+                    a = my_list[j]
             j += 1
         i -= 1
     return a
