134fb29ec02df55a0b6f93d0b1f9025077ab7e1d
Vixs101 <elijahvix695@gmail.com>
2024-03-20 22:08:18 +0100

    made a few changes to the task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,6 +2,6 @@
 def common_elements(set_1, set_2):
     if len(set_1) == 0 or len(set_2) == 0:
         return None
-    intersect = set_1.intersection(set_2)
+    intersect = set_1 & set_2
     return intersect
 
